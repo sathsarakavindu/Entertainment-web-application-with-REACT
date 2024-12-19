c3b04a232f6457872e678c1fc798574927d44cb1
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2024-12-19 20:02:26 +0530

    Implemented images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Slider from './Components/Slider';
 
 function App() {
   return (
     <div>
     <Header />
-    
+    <Slider/>
   </div>
   );
 }
